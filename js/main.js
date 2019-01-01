7bcbed139f1fed802ae23752e4d02e2e0abf1ecd
Tauhidul Islam <tauhidul.bitspeck@gmail.com>
2019-01-01 23:46:48 +0600

    sfsfsdf

--- js/main.js
+++ js/main.js
@@ -4,9 +4,45 @@
     'use strict';
 
 
-    /**owl carosel**/
-    
-
+    /*  =========================
+            Owl Carousel
+        ========================= */
+    /*---- main slider ----*/
+    var slider_pulse = $('.slider-wraper');
+    slider_pulse.owlCarousel({
+        navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+        loop: true,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 7000,
+        mouseDrag: true,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+    // Owl Carousel Animation
+    slider_pulse.on('translate.owl.carousel', function () {
+        $('.slider-info h2').removeClass('animated fadeInDown').css('opacity', '0');
+        $(".slider-info h3").removeClass("animated slideInRight").css("opacity", "0");
+        $('.slider-info p').removeClass('animated fadeInUp').css('opacity', '0');
+        $('.slider-info .button').removeClass('animated fadeIn').css('opacity', '0');
+    });
+    slider_pulse.on('translated.owl.carousel', function () {
+        $('.slider-info h2').addClass('animated fadeInDown').css('opacity', '1');
+        $(".slider-info h3").addClass("animated slideInRight").css("opacity", "1");
+        $('.slider-info p').addClass('animated fadeInUp').css('opacity', '1');
+        $('.slider-info .button').addClass('animated fadeIn').css('opacity', '1');
+    });
 
 
 
